5b6a02c6c3dc77fcdc851b571ba5cfc9f0017277
fcb2020 <giomakhatadze13@gmail.com>
2021-06-06 21:32:52 +0400

    push

---
+++ davaleba 7/js/main.js
@@ -0,0 +1,27 @@
+// function message() {
+//     confirm('გამარჯობა სტუმარო')
+// }
+
+// let btn = document.querySelector(".subm");
+
+// btn.addEventListener("click", function(){
+
+//     let price = 5;
+
+//     let weight = document.querySelector("#weight").value;
+//     let count = document.querySelector("#count").value;
+    
+//     let sum = ( price * Number(weight) ) * Number(count);
+//     output.innerText = sum + "₾";
+
+// })
+
+let munich = 25;
+
+let berlin = 75;
+
+let frankfurt = munich + berlin;
+
+document.getElementById("output"). innerHTML = frankfurt;
+
+console.log (frankfurt)
