a4cc265e4f5737a2f68a985f01c3879212a3e5f9
fcb2020 <giomakhatadze13@gmail.com>
2021-07-01 18:35:54 +0400

    push

---
+++ java 8/js/main.js
@@ -0,0 +1,17 @@
+let btnAdd = document.querySelector('#add');
+let btnSubtract = document.querySelector('#subtract');
+let input = document.querySelector('input');
+
+btnAdd.addEventListener('click', () => {
+  input.value = parseInt(input.value) + 1;
+
+});
+
+btnSubtract.addEventListener('click', () => {
+   input.value = parseInt(input.value) -  1;
+   if (qty.value <= 0) {
+       qty.value = 0;
+   }
+   
+    
+});
