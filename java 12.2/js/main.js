2fbe9ade01dc666729f1f39b8b61229b14c55743
fcb2020 <giomakhatadze13@gmail.com>
2021-07-19 19:07:51 +0400

    push

--- java 12.2/js/main.js
+++
@@ -1,3 +0,0 @@
-function togglePopup(){
-    document.getElementById("main1").classList.toggle("active");
-  }
---
+++ jquery 2/js/main.js
@@ -0,0 +1,75 @@
+$(".backbtn2").click(function (e) { 
+    e.preventDefault();
+    $('.regform').css({
+        'transform':'translateX(130%)',
+        'transition':'all .5s'
+    }) 
+    
+    
+    
+    
+});
+
+$(".backbtn2").click(function (e) { 
+     e.preventDefault();
+     $('#loginform').css({
+    'transform':'translateY(-125%)',
+    'transition':'all .1s'
+    
+ })
+    
+    
+});
+
+$(".backbtn2").click(function (e) { 
+    e.preventDefault();
+    $('#reg').css({
+        'transform':'translateY(-120%)',
+        'transition':'all .1s',
+
+    })
+    
+});
+
+$(".backbtn1").click(function (e) { 
+    e.preventDefault();
+    $('.regform').css({
+        'transform':'translateX(0)',
+        'transition':'all .5s',
+         
+    }) 
+    
+    
+});
+
+$(".backbtn1").click(function (e) { 
+    e.preventDefault();
+    $('#loginform').css({
+        'transform':'translateY(100%)',
+        'transition':'all .1s',
+         
+    }) 
+    
+    
+});
+
+$(".backbtn1").click(function (e) { 
+    e.preventDefault();
+    $('#reg').css({
+        'transform':'translateY(0%)',
+        'transition':'all .1s',
+         
+    }) 
+    
+    
+});
+
+
+
+
+
+
+
+
+   
+   
